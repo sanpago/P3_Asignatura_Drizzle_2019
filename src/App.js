3f07e3167416c89555a132b2f755cc4826244a95
Santiago Pavon <santiago@dit.upm.es>
2019-11-16 14:32:33 +0100

    Añadido drizzleConnect y comprueba inicializacion de Drizzle.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
 
 import React from 'react';
+import {drizzleConnect} from 'drizzle-react';
+import PropTypes from 'prop-types'
 
 import './App.css';
 
-const App = () => {
-    return (
-        <header className="App">
-            <h1>P3 - Asignatura</h1>
-        </header>
-    );
+
+const mapStateToProps = state => {
+    return {
+        drizzleStatus: state.drizzleStatus,
+        web3: state.web3
+    }
 }
 
-export default App;
+
+class App extends React.Component {
+
+    render() {
+        if (this.props.web3.status === 'failed') {
+            return ( // Display a web3 warning.
+                <main>
+                    <h1>⚠️</h1>
+                    <p>No connection to the Ethereum network. Please use the MetaMask extension.</p>
+                </main>
+            )
+        }
+
+        if (!this.props.drizzleStatus.initialized) {
+            return ( // Display a loading indicator.
+                <main>
+                    <h1>⚙️</h1>
+                    <p>Loading dapp...</p>
+                </main>
+            );
+        }
+
+        return (
+            <main>
+                <header className="App">
+                    <h1>P3 - Asignatura</h1>
+                </header>
+            </main>
+        );
+    }
+}
+
+App.contextTypes = {
+    drizzle: PropTypes.object
+};
+
+const AppContainer = drizzleConnect(App, mapStateToProps);
+
+export default AppContainer;
